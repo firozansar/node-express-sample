28e31908142ebafc84122df57f2dea1eabf8e447
firoz <firozansar@gmail.com>
2017-12-18 04:58:38 +0000

    changes

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
+});
+
 app.get('/', function(req, res) {
-	res.send('<html><head></head><body><h1>Hello world! This is Firoz!!</h1></body></html>');
+	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world! This is Firoz!</h1></body></html>');
 });
 
 app.get('/person/:id', function(req, res) {
@@ -12,7 +19,7 @@ app.get('/person/:id', function(req, res) {
 });
 
 app.get('/api', function(req, res) {
-	res.json({ firstname: 'John', lastname: 'Doe' });
+	res.json({ firstname: 'Firoz', lastname: 'Ansari' });
 });
 
 app.listen(port);
