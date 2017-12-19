f402eaf4c25464d49774483d53fb2186e9abe8cd
firoz <firozansar@gmail.com>
2017-12-19 19:40:04 +0000

    changes

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+//http://localhost:3000/person/5?qstr=hello
 app.get('/person/:id', function(req, res) {
 	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
 });
@@ -37,7 +38,7 @@ app.post('/personjson', jsonParser, function(req, res) {
 });
 
 app.get('/api', function(req, res) {
-	res.json({ firstname: 'John', lastname: 'Doe' });
+	res.json({ firstname: 'Firoz', lastname: 'Ansari' });
 });
 
 app.listen(port);
